4f651b52b60d5308d29ff97fd4bd9e44ba7bd5d8
vaishnaviuttarkar <vaishnaviuttarkar11@gmail.com>
2026-03-23 19:09:30 +0530

    wrote schema, table creation function

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -25,3 +25,46 @@ def create_schema(schema):
     conn.commit()
 
     close_conn_cursor(conn,cur)
+
+def create_table(schema):
+
+    conn, cur = get_conn_cursor()
+
+    if schema=='staging':
+        table_sql = f"""
+            CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                "Video_Title" TEXT NOT NULL,
+                "Upload_Date" TIMESTAMO NOT NULL,
+                "Duration" VARCHAR(20) NOT NULL,
+                "Video_Views" INT,
+                "Likes_Count" INT,
+                "Comments_Count" INT
+        );
+        """
+    else:
+        table_sql = f"""
+            CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                "Video_Title" TEXT NOT NULL,
+                "Upload_Date" TIMESTAMO NOT NULL,
+                "Duration" TIME NOT NULL,
+                "Video_Type" VARCHAR(10) NOT NULL,
+                "Video_Views" INT,
+                "Likes_Count" INT,
+                "Comments_Count" INT
+        );
+        """
+    
+    cur.execute(table_sql)
+
+    conn.commit()
+
+    close_conn_cursor(conn,cur)
+
+def get_video_ids(cur,schema):
+
+    cur.execute(f""" SELECT "Video_ID" FROM {schema}.{table}; """)
+    ids = cur.fetchall()
+    video_ids = [row['Video_ID'] for row in ids]
+    return video_ids
